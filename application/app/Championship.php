e36a967bd4ead4d0142bb18e191ba667ae4cb5a5
= <=>
2018-11-28 15:01:23 -0430

    subiendo los torneos

--- application/app/Championship.php
+++ application/app/Championship.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Championship extends Model
 {
-    protected $table = 'Championships';
+    protected $table = 'championships';
     protected $fillable = ['title','description','cover','datefrom','dateto','point_to_winner_game','total_points'];
 }
